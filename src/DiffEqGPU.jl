970058d9b884677d0a8eecc1552399fdc4ba332f
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-24 18:04:00 -0400

    clean up

--- src/DiffEqGPU.jl
+++ src/DiffEqGPU.jl
@@ -22,8 +22,7 @@ struct MonteGPUArray <: MonteArrayAlgorithm end
 function DiffEqBase.__solve(monteprob::DiffEqBase.AbstractMonteCarloProblem,
                  alg::Union{DiffEqBase.DEAlgorithm,Nothing},
                  montealg::MonteArrayAlgorithm;
-                 num_monte, batch_size = num_monte,
-                 pmap_batch_size = batch_size÷100 > 0 ? batch_size÷100 : 1, kwargs...)
+                 num_monte, batch_size = num_monte, kwargs...)
 
     probs = [monteprob.prob_func(deepcopy(monteprob.prob),i,1) for i in 1:num_monte]
     @assert all(p->p.tspan == probs[1].tspan,probs)
@@ -31,10 +30,10 @@ function DiffEqBase.__solve(monteprob::DiffEqBase.AbstractMonteCarloProblem,
 
     if montealg isa MonteGPUArray
         u0 = CuArray(hcat([probs[i].u0 for i in 1:num_monte]...))
-        p = CuArray(hcat([probs[i].p for i in 1:num_monte]...))
+        p  = CuArray(hcat([probs[i].p  for i in 1:num_monte]...))
     elseif montealg isa MonteCPUArray
         u0 = hcat([probs[i].u0 for i in 1:num_monte]...)
-        p = hcat([probs[i].p for i in 1:num_monte]...)
+        p  = hcat([probs[i].p  for i in 1:num_monte]...)
     end
 
     _f = let f=probs[1].f
@@ -45,7 +44,9 @@ function DiffEqBase.__solve(monteprob::DiffEqBase.AbstractMonteCarloProblem,
     end
 
     prob = ODEProblem(_f,u0,probs[1].tspan,p)
-    solve(prob,alg; kwargs...)
+    sol  = solve(prob,alg; kwargs...)
+
+
 end
 
 export MonteCPUArray, MonteGPUArray
