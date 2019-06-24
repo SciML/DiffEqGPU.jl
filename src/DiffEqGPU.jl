c1c0873dc6255e5be0578b43ad4d8c1a3d6ecad5
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-24 15:26:05 -0400

    first version

--- src/DiffEqGPU.jl
+++ src/DiffEqGPU.jl
@@ -1,5 +1,51 @@
 module DiffEqGPU
 
-greet() = print("Hello World!")
+using GPUifyLoops, CuArrays, CUDAnative, DiffEqBase, DiffEqMonteCarlo
+function gpu_kernel(f,du,u,p,t)
+    @loop for i in (1:size(u,2); (blockIdx().x-1) * blockDim().x + threadIdx().x)
+        @views @inbounds f(du[:,i],u[:,i],p,t)
+        nothing
+    end
+    nothing
+end
+
+function GPUifyLoops.launch_config(::typeof(gpu_kernel),maxthreads,context,g,f,du,u,args...;kwargs...)
+    t = min(maxthreads,size(u,2))
+    blocks = ceil(Int,size(u,2)/t)
+    (threads=t,blocks=blocks)
+end
+
+abstract type MonteArrayAlgorithm <: DiffEqBase.MonteCarloAlgorithm end
+struct MonteCPUArray <: MonteArrayAlgorithm end
+struct MonteGPUArray <: MonteArrayAlgorithm end
+
+function DiffEqBase.__solve(monteprob::DiffEqBase.AbstractMonteCarloProblem,
+                 alg::Union{DiffEqBase.DEAlgorithm,Nothing},
+                 montealg::MonteArrayAlgorithm;
+                 num_monte, batch_size = num_monte,
+                 pmap_batch_size = batch_size÷100 > 0 ? batch_size÷100 : 1, kwargs...)
+
+    probs = [monteprob.prob_func(deepcopy(monteprob.prob),i,1) for i in 1:num_monte]
+    @assert all(p->p.tspan == probs[1].tspan,probs)
+    #@assert all(p->p.f === probs[1].f,probs)
+
+    if montealg isa MonteGPUArray
+        u0 = CuArray(hcat([probs[i].u0 for i in 1:num_monte]...))
+        p = CuArray(hcat([probs[i].p for i in 1:num_monte]...))
+    elseif montealg isa MonteCPUArray
+        u0 = hcat([probs[i].u0 for i in 1:num_monte]...)
+        p = hcat([probs[i].p for i in 1:num_monte]...)
+    end
+
+    _f = let f=probs[1].f
+        function (du,u,p,t)
+            version = u isa CuArray ? CUDA() : CPU()
+            @launch version gpu_kernel(f,du,u,p,t)
+        end
+    end
+
+    prob = ODEProblem(_f,_u0,tspan,p)
+    solve(prob,alg; kwargs...)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
-using DiffEqGPU
-using Test
+using DiffEqGPU, OrdinaryDiffEq, Test
 
-@testset "DiffEqGPU.jl" begin
-    # Write your own tests here.
+#Performance check with nvvp
+
+function lorenz(du,u,p,t)
+ @inbounds begin
+     du[1] = 10.0f0(u[2]-u[1])
+     du[2] = u[1]*(28.0f0-u[3]) - u[2]
+     du[3] = u[1]*u[2] - (8/3f0)*u[3]
+ end
+ nothing
 end
+
+CuArrays.allowscalar(false)
+u0 = Float32[1.0;0.0;0.0]
+prob = ODEProblem(lorenz,u0,tspan)
+monteprob = MonteCarloProblem(prob)
+
+# CUDAnative.CUDAdrv.@profile
+@time solve(monteprob,Tsit5(),MonteGPUArray(),num_monte=100_000,saveat=1.0f0)
+@time solve(monteprob,Tsit5(),MonteThreads(), num_monte=100_000,saveat=1.0f0)
+@time solve(monteprob,Tsit5(),MonteSerial(), num_monte=100_000,saveat=1.0f0)
