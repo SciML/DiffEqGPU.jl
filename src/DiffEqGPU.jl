ed139c009026c410ae3ed2934a3e33163099e894
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-24 15:20:25 -0400

    Files generated by PkgTemplates

---
+++ src/DiffEqGPU.jl
@@ -0,0 +1,5 @@
+module DiffEqGPU
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DiffEqGPU
+using Test
+
+@testset "DiffEqGPU.jl" begin
+    # Write your own tests here.
+end
