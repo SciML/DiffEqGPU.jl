06ed9fdd2093484cb93ea8a14485b0ca4b7c66d0
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-16 04:17:38 -0400

    make test use parameters that are random

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using DiffEqGPU, CuArrays, OrdinaryDiffEq, Test
 
 function lorenz(du,u,p,t)
  @inbounds begin
-     du[1] = 10.0f0(u[2]-u[1])
-     du[2] = u[1]*(28.0f0-u[3]) - u[2]
-     du[3] = u[1]*u[2] - (8/3f0)*u[3]
+     du[1] = p[1]*(u[2]-u[1])
+     du[2] = u[1]*(p[2]-u[3]) - u[2]
+     du[3] = u[1]*u[2] - p[3]*u[3]
  end
  nothing
 end
@@ -12,8 +12,10 @@ end
 CuArrays.allowscalar(false)
 u0 = Float32[1.0;0.0;0.0]
 tspan = (0.0f0,100.0f0)
-prob = ODEProblem(lorenz,u0,tspan)
-monteprob = EnsembleProblem(prob)
+p = (10.0f0,28.0f0,8/3f0)
+prob = ODEProblem(lorenz,u0,tspan,p)
+prob_func = (prob,i,repeat) -> remake(prob,p=rand(Float32,3).*p)
+monteprob = EnsembleProblem(prob, prob_func = prob_func)
 
 #Performance check with nvvp
 # CUDAnative.CUDAdrv.@profile
