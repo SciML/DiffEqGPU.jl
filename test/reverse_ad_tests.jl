8d44e88cc7919ff887ba5f5cee66dd6ee6e59434
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-12-11 22:54:48 -0500

    add inbounds to indexing in modelf

--- test/reverse_ad_tests.jl
+++ test/reverse_ad_tests.jl
@@ -2,7 +2,9 @@ using OrdinaryDiffEq, DiffEqSensitivity, Flux, DiffEqGPU, CUDA, Test
 CUDA.allowscalar(false)
 
 function modelf(du,u,p,t)
-  du[1] = 1.01 * u[1] * p[1] * p[2]
+  @inbounds begin
+    du[1] = 1.01 * u[1] * p[1] * p[2]
+  end
 end
 
 function model()
