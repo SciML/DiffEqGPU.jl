52c34e07d3773071a4e7faefde30aa05775d89e2
Chris Rackauckas <accounts@chrisrackauckas.com>
2021-04-10 08:03:09 -0400

    add CUDAKernels

--- src/DiffEqGPU.jl
+++ src/DiffEqGPU.jl
@@ -5,7 +5,7 @@ module DiffEqGPU
 
 using DocStringExtensions
 using KernelAbstractions, CUDA, SciMLBase, DiffEqBase, LinearAlgebra, Distributed
-using KernelAbstractions: CUDADevice
+using CUDAKernels
 using CUDA: CuPtr, CU_NULL, Mem, CuDefaultStream
 using CUDA: CUBLAS
 using ForwardDiff
