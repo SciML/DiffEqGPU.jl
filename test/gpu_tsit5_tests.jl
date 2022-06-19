61eecebf6a4180d291e59e7f2798ba12698cb62c
Utkarsh <rajpututkarsh530@gmail.com>
2022-06-20 03:46:28 +0530

    some regression tests

--- test/gpu_tsit5_tests.jl
+++ test/gpu_tsit5_tests.jl
@@ -1,4 +1,4 @@
-using DiffEqGPU, OrdinaryDiffEq, StaticArrays
+using DiffEqGPU, OrdinaryDiffEq, StaticArrays, LinearAlgebra
 function lorenz(u, p, t)
     σ = p[1]
     ρ = p[2]
@@ -17,7 +17,15 @@ prob_func = (prob, i, repeat) -> remake(prob, p=p)
 monteprob = EnsembleProblem(prob, prob_func=prob_func, safetycopy=false)
 
 sol = solve(monteprob, GPUTsit5(), EnsembleGPUAutonomous(), trajectories=10, adaptive=false, dt=0.1f0)
-asol = solve(monteprob, GPUTsit5(), EnsembleGPUAutonomous(), trajectories=10, adaptive=true, dt=0.1f-1)
+asol = solve(monteprob, GPUTsit5(), EnsembleGPUAutonomous(), trajectories=10, adaptive=true, dt=0.1f-1,  abstol = 1f-8, reltol = 1f-5)
 
 @test sol.converged == true
 @test asol.converged == true
+
+## Regression test
+
+bench_sol = solve(prob, Tsit5(), adaptive=false, dt=0.1f0)
+bench_asol = solve(prob, Tsit5(), dt=0.1f-1, save_everystep = false, abstol = 1e-8, reltol = 1e-5)
+
+@show norm(bench_sol.u[end] - sol[1].u[end]) < 2e-2 
+@show norm(bench_asol.u[end] - asol[1].u[end]) < 1e-4
