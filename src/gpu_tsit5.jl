9cc16ac3f949ee267e8585b4d31ca0bcf4ee3e72
Utkarsh <rajpututkarsh530@gmail.com>
2022-06-20 03:25:30 +0530

    Integrator setup with adaptive GPUTsit5

--- src/gpu_tsit5.jl
+++ src/gpu_tsit5.jl
@@ -9,7 +9,7 @@ Adapt.adapt_structure(to, prob::ODEProblem{<:Any, <:Any, iip}) where {iip} =
     )
 
 
-
+## Fixed TimeStep Integrator
 mutable struct GPUTsit5Integrator{IIP, S, T, P, F} <: DiffEqBase.AbstractODEIntegrator{GPUTsit5, IIP, S, T}
     f::F                  # eom
     uprev::S              # previous state
@@ -37,8 +37,44 @@ const GPUT5I = GPUTsit5Integrator
 
 DiffEqBase.isinplace(::GPUT5I{IIP}) where {IIP} = IIP
 
+## Adaptive TimeStep Integrator
+
+mutable struct GPUATsit5Integrator{IIP, S, T, P, F, N, TOL, Q} <: DiffEqBase.AbstractODEIntegrator{GPUTsit5, IIP, S, T}
+    f::F                  # eom
+    uprev::S              # previous state
+    u::S                  # current state
+    tmp::S                # dummy, same as state
+    tprev::T              # previous time
+    t::T                  # current time
+    t0::T                 # initial time, only for reinit
+    tf::T
+    dt::T                 # step size
+    dtnew::T
+    tdir::T
+    p::P                  # parameter container
+    u_modified::Bool
+    k1::S         # interpolants of the algorithm
+    k2::S
+    k3::S
+    k4::S
+    k5::S
+    k6::S
+    k7::S
+    cs::SVector{6, T}     # ci factors cache: time coefficients
+    as::SVector{21, T}    # aij factors cache: solution coefficients
+    btildes::SVector{7,T}
+    rs::SVector{22, T}    # rij factors cache: interpolation coefficients
+    qold::Q
+    abstol::TOL
+    reltol::TOL
+    internalnorm::N       # function that computes the error EEst based on state
+end
+
+const GPUAT5I = GPUATsit5Integrator
+
+
 #######################################################################################
-# Initialization
+# Initialization of Integrators
 #######################################################################################
 @inline function gputsit5_init(f::F, IIP::Bool, u0::S, t0::T, dt::T, p::P) where {F, P, T, S<:AbstractArray{T}}
 
@@ -48,6 +84,17 @@ DiffEqBase.isinplace(::GPUT5I{IIP}) where {IIP} = IIP
 
     integ = GPUT5I{IIP, S, T, P, F}(f, copy(u0), copy(u0), copy(u0), t0, t0, t0, dt, sign(dt), p, true,
                                     copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), cs, as, rs)
+end
+
+@inline function gpuatsit5_init(f::F, IIP::Bool, u0::S, t0::T, tf::T, dt::T, p::P, abstol::TOL, reltol::TOL, internalnorm::N) where {F, P, S, T, N, TOL}
+
+    cs, as, btildes, rs = SimpleDiffEq._build_atsit5_caches(T)
+    
+    !IIP && @assert S <: SArray
+
+    qoldinit = eltype(S)(1e-4)
+
+    integ = GPUAT5I{IIP, S, T, P, F, N, TOL, typeof(qoldinit)}(f, copy(u0), copy(u0), copy(u0), t0, t0, t0, tf, dt, dt, sign(tf-t0), p, true, copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), copy(u0), cs, as, btildes, rs, qoldinit, abstol, reltol, internalnorm)
 end
 ## GPU solver
 
@@ -235,6 +282,90 @@ function build_adaptive_tsit5_controller_cache(::Type{T}) where {T}
     return beta1, beta2, qmax, qmin, gamma, qoldinit, qold
 end
 
+
+@inline function step!(integ::GPUAT5I{false, S, T}) where {S, T}
+    
+    beta1, beta2, qmax, qmin, gamma, qoldinit , _ = build_adaptive_tsit5_controller_cache(eltype(integ.u))
+    c1, c2, c3, c4, c5, c6 = integ.cs;
+    dt = integ.dtnew; t = integ.t; p = integ.p; tf = integ.tf
+    a21, a31, a32, a41, a42, a43, a51, a52, a53, a54,
+    a61, a62, a63, a64, a65, a71, a72, a73, a74, a75, a76 = integ.as
+    btilde1, btilde2, btilde3, btilde4, btilde5, btilde6, btilde7 = integ.btildes
+
+    tmp = integ.tmp; f = integ.f
+    integ.uprev = integ.u; uprev = integ.u
+
+    qold = integ.qold
+    abstol = integ.abstol
+    reltol = integ.reltol
+
+    if integ.u_modified
+      k1 = f(uprev, p, t)
+      integ.u_modified=false
+    else
+      @inbounds k1 = integ.k7;
+    end
+
+    EEst = Inf
+
+    while EEst > 1.0
+        dt < 1e-14 && error("dt<dtmin")
+
+        tmp = uprev + dt * a21 * k1
+        k2 = f(tmp, p, t + c1 * dt)
+        tmp = uprev + dt * (a31 * k1 + a32 * k2)
+        k3 = f(tmp, p, t + c2 * dt)
+        tmp = uprev + dt * (a41 * k1 + a42 * k2 + a43 * k3)
+        k4 = f(tmp, p, t + c3 * dt)
+        tmp = uprev + dt * (a51 * k1 + a52 * k2 + a53 * k3 + a54 * k4)
+        k5 = f(tmp, p, t + c4 * dt)
+        tmp = uprev + dt * (a61 * k1 + a62 * k2 + a63 * k3 + a64 * k4 + a65 * k5)
+        k6 = f(tmp, p, t + dt)
+        u = uprev + dt * (a71 * k1 + a72 * k2 + a73 * k3 + a74 * k4 + a75 * k5 + a76 * k6)
+        k7 = f(u, p, t + dt)
+
+        tmp = dt * (btilde1 * k1 + btilde2 * k2 + btilde3 * k3 + btilde4 * k4 +
+                    btilde5 * k5 + btilde6 * k6 + btilde7 * k7)
+        tmp = tmp ./ (abstol .+ max.(abs.(uprev), abs.(u)) * reltol)
+        EEst = DiffEqBase.ODE_DEFAULT_NORM(tmp, t)
+
+        if iszero(EEst)
+            q = inv(qmax)
+        else
+            q11 = EEst^beta1
+            q = q11 / (qold^beta2)
+        end
+
+        if EEst > 1
+            dt = dt / min(inv(qmin), q11 / gamma)
+        else # EEst <= 1
+            q = max(inv(qmax), min(inv(qmin), q / gamma))
+            qold = max(EEst, qoldinit)
+            dtnew = dt/q #dtnew
+            dtnew = min(abs(dtnew),abs(tf-t-dt))
+    
+            @inbounds begin # Necessary for interpolation
+                integ.k1 = k1; integ.k2 = k2; integ.k3 = k3
+                integ.k4 = k4; integ.k5 = k5; integ.k6 = k6
+                integ.k7 = k7;
+            end
+    
+            integ.dt = dt
+            integ.dtnew = dtnew
+            integ.qold = qold
+            integ.tprev = t
+            integ.u = u
+    
+            if (tf - t - dt) < 1e-14
+              integ.t = tf
+            else
+              integ.t += dt
+            end
+        end
+    end
+    return nothing
+end
+
 function atsit5_kernel(_prob, ps, _us, _ts, dt, abstol, reltol,
     ::Val{saveat}, ::Val{save_everystep}) where {saveat,save_everystep}
     i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
@@ -276,6 +407,8 @@ function atsit5_kernel(_prob, ps, _us, _ts, dt, abstol, reltol,
     u = u0
     k7 = f(u, p, t)
 
+    integ = gpuatsit5_init(prob.f, false, prob.u0, prob.tspan[1], prob.tspan[2], dt, prob.p, abstol, reltol, DiffEqBase.ODE_DEFAULT_NORM)
+    step!(integ)
     cs, as, btildes, rs = SimpleDiffEq._build_atsit5_caches(eltype(u0))
     c1, c2, c3, c4, c5, c6 = cs
     a21, a31, a32, a41, a42, a43, a51, a52, a53, a54,
@@ -284,74 +417,25 @@ function atsit5_kernel(_prob, ps, _us, _ts, dt, abstol, reltol,
 
     beta1, beta2, qmax, qmin, gamma, qoldinit, qold = build_adaptive_tsit5_controller_cache(eltype(u0))
 
-    while t < tspan[2]
-        uprev = u
-        k1 = k7
-        EEst = Inf
-
-        while EEst > 1.0
-            dt < 1e-14 && error("dt<dtmin")
-
-            tmp = uprev + dt * a21 * k1
-            k2 = f(tmp, p, t + c1 * dt)
-            tmp = uprev + dt * (a31 * k1 + a32 * k2)
-            k3 = f(tmp, p, t + c2 * dt)
-            tmp = uprev + dt * (a41 * k1 + a42 * k2 + a43 * k3)
-            k4 = f(tmp, p, t + c3 * dt)
-            tmp = uprev + dt * (a51 * k1 + a52 * k2 + a53 * k3 + a54 * k4)
-            k5 = f(tmp, p, t + c4 * dt)
-            tmp = uprev + dt * (a61 * k1 + a62 * k2 + a63 * k3 + a64 * k4 + a65 * k5)
-            k6 = f(tmp, p, t + dt)
-            u = uprev + dt * (a71 * k1 + a72 * k2 + a73 * k3 + a74 * k4 + a75 * k5 + a76 * k6)
-            k7 = f(u, p, t + dt)
-
-            tmp = dt * (btilde1 * k1 + btilde2 * k2 + btilde3 * k3 + btilde4 * k4 +
-                        btilde5 * k5 + btilde6 * k6 + btilde7 * k7)
-            tmp = tmp ./ (abstol .+ max.(abs.(uprev), abs.(u)) * reltol)
-            EEst = DiffEqBase.ODE_DEFAULT_NORM(tmp, t)
-
-            if iszero(EEst)
-                q = inv(qmax)
-            else
-                q11 = EEst^beta1
-                q = q11 / (qold^beta2)
-            end
-
-            if EEst > 1
-                dt = dt / min(inv(qmin), q11 / gamma)
-            else # EEst <= 1
-                q = max(inv(qmax), min(inv(qmin), q / gamma))
-                qold = max(EEst, qoldinit)
-                dtold = dt
-                dt = dt / q #dtnew
-                dt = min(abs(dt), abs(tf - t - dtold))
-                told = t
-                if (tf - t - dtold) < 1e-14
-                    t = tf
-                else
-                    t += dtold
-                end
-
-                if saveat === nothing && save_everystep
-                    error("Do not use saveat == nothing & save_everystep = true in adaptive version")
-                else saveat !== nothing
-                    while cur_t <= length(ts) && ts[cur_t] <= t
-                        savet = ts[cur_t]
-                        θ = (savet - told) / dtold
-                        b1θ, b2θ, b3θ, b4θ, b5θ, b6θ, b7θ = SimpleDiffEq.bθs(rs, θ)
-                        us[cur_t] = uprev + dtold * (
-                            b1θ * k1 + b2θ * k2 + b3θ * k3 + b4θ * k4 + b5θ * k5 + b6θ * k6 + b7θ * k7)
-                        cur_t += 1
-                    end
-                end
-
+    while integ.t < tspan[2]
+        step!(integ)
+        if saveat === nothing && save_everystep
+            error("Do not use saveat == nothing & save_everystep = true in adaptive version")
+        else saveat !== nothing
+            while cur_t <= length(ts) && ts[cur_t] <= integ.t
+                savet = ts[cur_t]
+                θ = (savet - integ.tprev) / integ.dt
+                b1θ, b2θ, b3θ, b4θ, b5θ, b6θ, b7θ = SimpleDiffEq.bθs(integ.rs, θ)
+                us[cur_t] = integ.uprev + integ.dt * (
+                    b1θ * integ.k1 + b2θ * integ.k2 + b3θ * integ.k3 + b4θ * integ.k4 + b5θ * integ.k5 + b6θ * integ.k6 + b7θ * integ.k7)
+                cur_t += 1
             end
         end
     end
 
     if !saveat && !save_everystep
-        @inbounds us[2] = u
-        @inbounds ts[2] = t
+        @inbounds us[2] = integ.u
+        @inbounds ts[2] = integ.t
     end
 
     return nothing
