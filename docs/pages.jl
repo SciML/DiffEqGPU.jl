b0b5c939505d893547883729d77de3060e1b3fd8
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-12-14 11:45:13 -0500

    format

--- docs/pages.jl
+++ docs/pages.jl
@@ -2,21 +2,15 @@
 
 pages = ["index.md",
     "getting_started.md",
-    "Tutorials" =>
-        "GPU Ensembles" => Any["tutorials/gpu_ensemble_basic.md",
-                       "tutorials/parallel_callbacks.md",
-                       "tutorials/multigpu.md"],
-        "Within-Method GPU" => Any[
-            "tutorials/within_method_gpu.md",
-        ],
-    "Examples" => Any[
-        "GPU Ensembles" => Any["examples/sde.md",
-                      "examples/ad.md",
-                      "examples/reductions.md"],
-        "Within-Method GPU" => Any[
-            "examples/reaction_diffusion.md",
-            "examples/bruss.md",
-        ],
+    "Tutorials" => Any["GPU Ensembles" => Any["tutorials/gpu_ensemble_basic.md",
+                                              "tutorials/parallel_callbacks.md",
+                                              "tutorials/multigpu.md"],
+                       "Within-Method GPU" => Any["tutorials/within_method_gpu.md"]],
+    "Examples" => Any["GPU Ensembles" => Any["examples/sde.md",
+                                             "examples/ad.md",
+                                             "examples/reductions.md"],
+                      "Within-Method GPU" => Any["examples/reaction_diffusion.md",
+                                                 "examples/bruss.md"]],
     "Manual" => Any["manual/ensemblegpukernel.md",
                     "manual/ensemblegpuarray.md",
                     "manual/optimal_trajectories.md",
