8801223d8ba23668f85177f19958ed7ad5367d15
Tom Short <tshort.rlists@gmail.com>
2023-05-27 07:01:21 -0400

    Support mass_matrix for rosenbrock23

--- src/perform_step/gpu_rosenbrock23_perform_step.jl
+++ src/perform_step/gpu_rosenbrock23_perform_step.jl
@@ -38,7 +38,8 @@
     J = f.jac(uprev, p, t)
     dT = f.tgrad(uprev, p, t)
 
-    W = I - γ * J
+    mass_matrix = integ.f.mass_matrix
+    W = mass_matrix - γ * J
     W_fact = W
 
     # F = lu(W)
@@ -46,8 +47,12 @@
     k1 = W_fact \ (F₀ + γ * dT)
 
     F₁ = f(uprev + dto2 * k1, p, t + dto2)
-
-    k2 = W_fact \ (F₁ - k1) + k1
+    
+    if mass_matrix === I
+        k2 = W_fact \ (F₁ - k1) + k1
+    else
+        k2 = W_fact \ (F₁ - mass_matrix * k1) + k1
+    end
 
     integ.u = uprev + dt * k2
 
@@ -148,6 +153,8 @@ end
 
     EEst = convert(T, Inf)
 
+    mass_matrix = integ.f.mass_matrix
+
     while EEst > convert(T, 1.0)
         dt < convert(T, 1.0f-14) && error("dt<dtmin")
 
@@ -159,7 +166,7 @@ end
         J = f.jac(uprev, p, t)
         dT = f.tgrad(uprev, p, t)
 
-        W = I - γ * J
+        W = mass_matrix - γ * J
         W_fact = W
 
         # F = lu(W)
@@ -168,13 +175,24 @@ end
 
         F₁ = f(uprev + dto2 * k1, p, t + dto2)
 
-        k2 = W_fact \ (F₁ - k1) + k1
+        if mass_matrix === I
+            k2 = W_fact \ (F₁ - k1) + k1
+        else
+            k2 = W_fact \ (F₁ - mass_matrix * k1) + k1
+        end
 
         u = uprev + dt * k2
 
         e32 = T(6) + sqrt(T(2))
         F₂ = f(u, p, t + dt)
-        k3 = W_fact \ (F₂ - e32 * (k2 - F₁) - 2 * (k1 - F₀) + dt * dT)
+        
+        if mass_matrix === I
+            k3 = W_fact \ (F₂ - e32 * (k2 - F₁) - 2 * (k1 - F₀) + dt * dT)
+
+       else
+            k3 = W_fact \ (F₂ - mass_matrix * (e32 * k2 + 2 * k1) +
+                           e32 * F₁ + 2 * F₀ + dt * dT)
+        end
 
         tmp = dto6 * (k1 - 2 * k2 + k3)
         tmp = tmp ./ (abstol .+ max.(abs.(uprev), abs.(u)) * reltol)
