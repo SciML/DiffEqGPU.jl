88fb3c84f572e0083f36b066358de37ce5b0e717
Utkarsh <rajpututkarsh530@gmail.com>
2023-09-09 19:02:47 -0400

    Add utils.jl

---
+++ src/utils.jl
@@ -0,0 +1,6 @@
+diffeqgpunorm(u::AbstractArray, t) = sqrt.(sum(abs2, u) ./ length(u))
+diffeqgpunorm(u::Union{AbstractFloat, Complex}, t) = abs(u)
+function diffeqgpunorm(u::AbstractArray{<:ForwardDiff.Dual}, t)
+    sqrt.(sum(abs2 ∘ ForwardDiff.value, u) ./ length(u))
+end
+diffeqgpunorm(u::ForwardDiff.Dual, t) = abs(ForwardDiff.value(u))
